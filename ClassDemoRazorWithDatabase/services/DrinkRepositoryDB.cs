b7d0da558890b6c39287549f0a480b5366f4d5b4
Peter Levinsky <pele@zealand.dk>
2024-03-11 10:01:02 +0100

    bla

--- ClassDemoRazorWithDatabase/services/DrinkRepositoryDB.cs
+++ ClassDemoRazorWithDatabase/services/DrinkRepositoryDB.cs
@@ -1,4 +1,5 @@
 ﻿using ClassDemoRazorWithDatabase.model;
+using Microsoft.Data.SqlClient;
 
 namespace ClassDemoRazorWithDatabase.services
 {
@@ -16,7 +17,34 @@ namespace ClassDemoRazorWithDatabase.services
 
         public List<Drink> GetAll()
         {
-            throw new NotImplementedException();
+            List<Drink> drinks = new List<Drink>();
+
+            SqlConnection connection = new SqlConnection(Secret.ConnectionString);
+            connection.Open();
+
+            String sql = "select * from Drink";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Drink drink = ReadDrink(reader);
+                drinks.Add(drink);
+            }
+
+            connection.Close();
+            return drinks;
+        }
+
+        private Drink ReadDrink(SqlDataReader reader)
+        {
+            Drink drink = new Drink();
+
+            drink.Id = reader.GetInt32(0);
+            drink.Name = reader.GetString(1);
+            drink.Alc = reader.GetBoolean(2);
+
+            return drink;
         }
 
         public Drink GetById(int id)
