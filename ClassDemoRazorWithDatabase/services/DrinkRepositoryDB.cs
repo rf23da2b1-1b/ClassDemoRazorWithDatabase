9f1c0399f7335b9af38479090dd7c4a8a71ac641
Peter Levinsky <pele@zealand.dk>
2024-03-11 12:53:04 +0100

    Med de sidste tre metoder

--- ClassDemoRazorWithDatabase/services/DrinkRepositoryDB.cs
+++ ClassDemoRazorWithDatabase/services/DrinkRepositoryDB.cs
@@ -1,11 +1,14 @@
 ﻿using ClassDemoRazorWithDatabase.model;
 using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
 
 namespace ClassDemoRazorWithDatabase.services
 {
     public class DrinkRepositoryDB : IDrinkRepo
     {
-
+        /*
+         * CREATE
+         */
         private const String insertSql = "insert into Drink values(@name,@alc)";
         public Drink Add(Drink newDrink)
         {
@@ -16,17 +19,19 @@ namespace ClassDemoRazorWithDatabase.services
             cmd.Parameters.AddWithValue("@name", newDrink.Name);
             cmd.Parameters.AddWithValue("@alc", newDrink.Alc);
 
-            int row = cmd.ExecuteNonQuery();
-            Console.WriteLine("Rows affected " + row);
-
+            int rows = cmd.ExecuteNonQuery();
+            if (rows == 0)
+            {
+                throw new ArgumentException("Kunne ikke oprette drink =" + newDrink);
+            }
             connection.Close();
             return newDrink;
         }
 
-        public Drink Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        /*
+         * READ
+         */
+        private const String selectAllSql = "select * from Drink";
 
         public List<Drink> GetAll()
         {
@@ -35,8 +40,7 @@ namespace ClassDemoRazorWithDatabase.services
             SqlConnection connection = new SqlConnection(Secret.ConnectionString);
             connection.Open();
 
-            String sql = "select * from Drink";
-            SqlCommand cmd = new SqlCommand(sql, connection);
+            SqlCommand cmd = new SqlCommand(selectAllSql, connection);
 
             SqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
@@ -60,14 +64,85 @@ namespace ClassDemoRazorWithDatabase.services
             return drink;
         }
 
+        private const String selectByIdlSql = "select * from Drink where Id = @Id";
         public Drink GetById(int id)
         {
-            throw new NotImplementedException();
+            SqlConnection connection = new SqlConnection(Secret.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(selectByIdlSql, connection);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            Drink drink = null;
+            if (reader.Read())
+            {
+                drink = ReadDrink(reader);
+            }
+            else
+            {
+                // no row i.e. not found
+                throw new KeyNotFoundException();
+            }
+
+            connection.Close();
+            return drink;
         }
 
+        /*
+         * UPDATE
+         */
+
+        private const String updateSql = "update Drink set Name=@name, Alk=@alc where Id=@id";
         public Drink Update(int id, Drink updatedDrink)
         {
-            throw new NotImplementedException();
+            if (id != updatedDrink.Id)
+            {
+                throw new ArgumentException("Kan ikke opdatere id er forskellig fra id i updatedeDrink");
+            }
+
+            SqlConnection connection = new SqlConnection(Secret.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(insertSql, connection);
+            cmd.Parameters.AddWithValue("@name", updatedDrink.Name);
+            cmd.Parameters.AddWithValue("@alc", updatedDrink.Alc);
+            cmd.Parameters.AddWithValue("@Id", updatedDrink.Id);
+
+            int row = cmd.ExecuteNonQuery();
+            Console.WriteLine("Rows affected " + row);
+
+            connection.Close();
+            return updatedDrink;
+        }
+
+
+
+
+        /*
+         * DELETE
+         */
+        private const String deleteByIdlSql = "delete from Drink where Id = @Id";
+        public Drink Delete(int id)
+        {
+            Drink drink = GetById(id);
+
+            SqlConnection connection = new SqlConnection(Secret.ConnectionString);
+            connection.Open();
+
+            SqlCommand cmd = new SqlCommand(deleteByIdlSql, connection);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            int rows = cmd.ExecuteNonQuery();
+
+            if (rows == 0)
+            {
+                throw new ArgumentException("Kunne ikke slette drink med id=" + id);
+            }
+            connection.Close();
+
+            return drink;
         }
     }
 }
